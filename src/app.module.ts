3c7a6cf283a4df41f086c49188beead2da632d1a
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-28 00:39:24 +0900

    user entity, postgresql 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/user_entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '', //나중에 git 설정 후 비밀번호 설정
+      database: 'career_teen',
+      entities: [User],
+      synchronize: true, //자동 생성 수정
+    }),
+    TypeOrmModule.forFeature([User]), //user entity
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user_entity.ts
@@ -0,0 +1,40 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'; //npm install typeorm
+
+@Entity()
+export class User{ //클래스명 대문자 시작
+
+    static readonly Role = {
+        Mentor: 'mentor',
+        Mentee: 'mentee',
+    } as const;
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column()
+    passwd: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    age: number; //age string -> number로 변경
+
+    @Column()
+    job: string;
+
+    @Column()
+    resolution: string;
+
+    @Column({ type: 'enum', enum: User.Role })
+    role: (typeof User.Role)[keyof typeof User.Role];
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
