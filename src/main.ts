0298dac16986fb134711d7de03fcd55a2a3feff5
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-29 14:43:02 +0900

    토큰 설정 해결, 회원가입 성공

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { JwtAuthGuard } from './auth/jwt.auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +13,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.useGlobalGuards(new JwtAuthGuard());
-
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
