e2c2074c7a621bfe72cd99a0c9ee606fb7046d98
jiy0712 <s2325@e-mirim.hs.kr>
2025-06-16 15:06:44 +0900

    railway와 PostgreSQL 연결

--- src/BulletinBoard/BulletinBoard.module.ts
+++ src/BulletinBoard/BulletinBoard.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BulletinBoard } from './BulletinBoard.entity';
 import { BulletinService } from './BulletinBoard.service';
 import { BulletinController } from './BulletinBoard.controller';
+import { UserModule } from '../User/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BulletinBoard])],
+  imports: [TypeOrmModule.forFeature([BulletinBoard]), UserModule],
   providers: [BulletinService],
   controllers: [BulletinController],
 })
