c8cf88089246c452385c17d97b648d5c7c674515
jiy0712 <s2325@e-mirim.hs.kr>
2025-06-19 00:43:08 +0900

    jwt 로그 추가

--- src/auth/jwt.auth.guard.ts
+++ src/auth/jwt.auth.guard.ts
@@ -6,16 +6,24 @@ import { Request } from 'express';
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
     const request: Request = context.switchToHttp().getRequest<Request>();
+    const { url, method, headers } = request;
 
-    const { url, method } = request;
+    console.log('[JwtAuthGuard] 요청 URL:', url);
+    console.log('[JwtAuthGuard] 요청 METHOD:', method);
+    console.log(
+      '[JwtAuthGuard] 요청 Authorization 헤더:',
+      headers.authorization,
+    );
 
     if (
       (url === '/auth/login' && method === 'POST') ||
       (url === '/users' && method === 'POST')
     ) {
+      console.log('[JwtAuthGuard] 인증 없이 접근 허용됨');
       return true;
     }
 
+    console.log('[JwtAuthGuard] 인증 토큰 확인 중...');
     return super.canActivate(context);
   }
 }
