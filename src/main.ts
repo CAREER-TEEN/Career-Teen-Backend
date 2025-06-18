8476b93b843bde3f5cbe0aa31e2f9dfee9715ddf
jiy0712 <s2325@e-mirim.hs.kr>
2025-06-19 03:23:42 +0900

    cors 설정

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['*'], //이거 프론트 도메인 넣는거니 이케 해야겠지 지금은?
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   console.log('DB_HOST:', process.env.DB_HOST);
   console.log('DB_PORT:', process.env.DB_PORT);
   console.log('DB_USERNAME:', process.env.DB_USERNAME);
